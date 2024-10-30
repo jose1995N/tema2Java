8a808ae5fb2f1a5c762060f966333e86e4e1a396
jose1995N <jmatmor313@g.educaand.es>
2024-10-30 13:38:46 +0100

    Add files via upload

---
+++ ejecicio1.java
@@ -0,0 +1,24 @@
+public class ejecicio1 {
+    public static void main(String[] args) {
+        int p = 1;
+        int q = 2;
+        float x= 1.2;
+        float y=3.4;
+        float z = 4.5;
+        char  a = Hola ;
+        char b = Mundo;
+        char c = hello;
+        double raiz1= 2;
+        double raiz2 = 5;
+        long contador = 456;
+        byte  indicador = 5;
+        int = indice = 1, 2;
+        double precio =5.3;
+        double precioFinal = 4;
+        char car1
+        char car2
+        byte valor
+        long primero, ultimo
+        String nombre= jose;
+    }
+}
