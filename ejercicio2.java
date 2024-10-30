c7d39f00434677e44a23892b279131a2030bde87
jose1995N <jmatmor313@g.educaand.es>
2024-10-30 13:49:54 +0100

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,25 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        byte var Edad;
+       long  var CodigoPostal;
+       byte var Altura;
+       byte  var Genero (valor=hombre, valor= mujer);
+        char var Nombre;
+        byte var NumeeroDehijos;
+        long var iva;
+       long  var tallaCamisa;
+       long  var Peso;
+       long var Precio;
+       int Var AlumnoRepetidor
+      char  Var mensaje
+       char  Var letra
+       int  var mayorEdad
+        byte var minutos 
+        byte var dias
+        long var MarticulaCoche
+        byte var contador
+       byte Var mayorDeEdad 
+        byte var tallaCamisetas
+
+    }
+}
