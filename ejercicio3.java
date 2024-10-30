2b3f8becefaffa0e79c9e04466b0c44f98e6ac57
jose1995N <jmatmor313@g.educaand.es>
2024-10-30 14:02:09 +0100

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,37 @@
+public class ejercicio3 {
+    public static void main(String[] args) {
+        float resultado1;
+        resultado1 = (3/2+4/3);
+
+        float resultado2;
+        resultado2 = (1/(x-5)-3*x*y/4 );
+
+        float resultado3;
+        resultado3 = (1/2+7);
+
+        float resultado4;
+        resultado4 = (7+1/2 )
+
+        float resultado5;
+        resultado5 = (a*a/(b-c)+(d-e)/(f-g*h/j) );
+
+        float resultado6;
+        resultado6 =(m/n+p );
+
+        float resultado7;
+        resultado7 = (m+n/(p-q))
+
+        float resultado8;
+        resultado8 = (a*a/(b*b)+c*c/(d*d))
+
+        float resultado9;
+        resultado9 = ((m+n/p)/(q-r/s))
+
+        float resultado10;
+        resultado10 =((3*a+b)/(c-(d+5*e)/(f+g/(2*h)))) 
+
+        float resultado11;
+        resultado11 =((a*a+2*a*b+b*b)/(1/(x*x))+2)
+
+    }
+}
